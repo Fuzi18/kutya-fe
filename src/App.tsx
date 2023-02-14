fbd491d2768e7b1d09c5cbb366596e67bbeda558
Fuzi18 <fuzesi.t2003@]mail.com>
2023-02-14 12:19:45 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+interface State {
+  kutyak : Kutya[];
+  ujNev: string;
+  ujKor: number;
+}
+
+interface Kutya {
+  id: number;
+  nev: string;
+  kor : number;
+}
+
+
+class App extends Component<{}, State> {
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      ujNev: '',
+      ujKor: 0,
+      kutyak: [],
+    }
+  }
+
+
+  async Kutyabetoltes() {
+    let response = await fetch('http://localhost:3000/api/kutya');
+    let data = await response.json() as Kutya[];
+    this.setState({
+      kutyak: data,
+    })
+  }
+
+  componentDidMount() {
+    this.Kutyabetoltes();
+  }
+
+
+  Felvetelkezeles = async () => {
+    const { ujNev, ujKor} = this.state;
+    if(ujNev.trim() === '' || ujKor <=0) {
+     return;
+    }
+
+    const adat = {
+      nev: ujNev,
+      kor: ujKor,
+    };
+
+    await fetch('http://localhost:3000/api/kutya', {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(adat),
+    });
+
+    this.setState({
+      ujNev: '',
+      ujKor: 0,
+    })
+
+    await this.Kutyabetoltes();
+
+  }
+
+
+  render(){
+    const { ujNev, ujKor } = this.state;
+
+    return <div>
+      <h2>Új Kutya hozzáadása</h2>
+      Név: <input type='text' value={ujNev} onChange={e => this.setState({ujNev: e.currentTarget.value})}></input><br/>
+      Kor: <input type='number' value={ujKor} onChange={e => this.setState({ ujKor: parseInt(e.currentTarget.value)})}></input><br/>
+      <button onClick={this.Felvetelkezeles}>Felvétel</button>
+      <h2>Kutyák</h2>
+      <ul>
+        {
+          this.state.kutyak.map((kutya) => <li>{kutya.nev} | {kutya.kor} </li>)
+        }
+      </ul>
     </div>
-  );
+  }
 }
 
 export default App;
